93c00c57d8b075ab950be6704f976d8b584fa09b
Daniel Pausackerl <daniel.pausackerl@edu.campus02.at>
2016-02-09 20:55:32 +0100

    upload

---
+++ src/KleinesEinMalEins.java
@@ -0,0 +1,15 @@
+
+public class KleinesEinMalEins {
+
+	public static void main(String[] args) {
+
+		for (int a = 1; a <= 10; a++) {
+			for (int b = 1; b <= 10; b++) {
+				System.out.print(a * b + "\t");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
